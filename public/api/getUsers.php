b0a8431b2ef9a36524ddac951bfd720d1b3b80d3
Francisco <pikino84@gmail.com>
2023-08-30 21:35:13 +0000

    se excluyo del api la conexion getUsers

--- public/api/getUsers.php
+++ public/api/getUsers.php
@@ -4,17 +4,10 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
 
-<<<<<<< HEAD
 $servername = "sqlexterno.database.windows.net";
 $username = "israel";
 $password = "Learsi01@";
 $dbname = "COMERCIALDB";
-=======
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "splendor";
->>>>>>> 8840d5626644abd190104368f2bc28cec15e0c6f
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
