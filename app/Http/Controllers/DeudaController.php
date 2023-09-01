bcc9efc48fdfd12aa62b9e2094a300008dd10c0f
pikino84 <pikino84@gmail.com>
2023-09-01 02:03:35 -0500

    fix error typo

--- app/Http/Controllers/DeudaController.php
+++ app/Http/Controllers/DeudaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\Deuda;
-use APP\Models\Invoice;
+use App\Models\Invoice;
 
 
 class DeudaController extends Controller
